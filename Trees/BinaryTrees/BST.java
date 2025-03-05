cb5e28caa492587d0e9b2696df6a46ee0c9214d5
harshitmathur69 <harshitmathur88@gmail.com>
2025-03-05 19:20:40 +0530

    Project added!

---
+++ Trees/BinaryTrees/BST.java
@@ -0,0 +1,98 @@
+package Trees.BinaryTrees;
+
+public class BST{
+    public class Node{
+        private int value;
+        private Node left;
+        private Node right;
+        int height;
+
+        public Node(int value){
+            this.value = value;
+        }
+        public int getValue(){
+            return value;
+        }
+    }
+    private Node root;
+    public  BST(){
+
+    }
+
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(int value){
+        root = insert(value, root);
+    }
+
+    private Node insert(int value,Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+
+    public void populate(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populateSorted(int[] nums){
+        populateSorted(nums,0,nums.length);
+    }
+
+    private void populateSorted(int[] nums,int start,int end){
+        if(start >= end){
+            return;
+        }
+        int mid = (start + end)/2;
+        this.insert(nums[mid]);
+        populateSorted(nums,start,mid);
+        populateSorted(nums,mid+1,end);
+    }
+
+    public boolean balanced(){
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node){
+        if(node == null){
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display(){
+        display(this.root,"Root node : ");
+    }
+
+    private void display(Node node,String details){
+        if(node == null){
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left,"Left Child of " + node.value + " : ");
+        display(node.right,"Right Child of " + node.value + " : ");
+    }
+
+}
